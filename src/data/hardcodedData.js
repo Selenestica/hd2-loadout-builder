2b9302e5be7a72d49b051ab930dd0c8acdf82929
PeterDavids <peter@rawstadia.com>
2024-04-12 13:53:27 +0200

    second commit lol

---
+++ src/data/hardcodedData.js
@@ -0,0 +1,27 @@
+export const primaryWeaponData = {
+
+}
+
+export const secondaryWeaponData = {
+
+}
+
+export const grenadeData = {
+
+}
+
+export const armorData = {
+
+}
+
+export const helmetData = {
+
+}
+
+export const strategemData = {
+
+}
+
+export const boosterData = {
+
+}
