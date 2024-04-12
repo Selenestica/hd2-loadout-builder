af2eee0c1b4a655945cdc3eaaed14caea4b22f46
PeterDavids <peter@rawstadia.com>
2024-04-12 14:13:10 +0200

    added strategem assets

--- src/data/hardcodedData.js
+++ src/data/hardcodedData.js
@@ -1,27 +1,23 @@
-export const primaryWeaponData = {
+export const primaryWeaponData = []
 
-}
+export const secondaryWeaponData = []
 
-export const secondaryWeaponData = {
+export const grenadeData = []
 
-}
+export const armorData = []
 
-export const grenadeData = {
+export const helmetData = []
 
-}
+export const strategemData = [
+    {
+        id: 1,
+        name: "MG-43 Machine Gun",
+        range: {short: 1, medium: 1, long: 0},
+        coverage: {
+            bots: {'light': 1, 'light-medium': 1, },
+            bugs: {}
+        }
+    },
+]
 
-export const armorData = {
-
-}
-
-export const helmetData = {
-
-}
-
-export const strategemData = {
-
-}
-
-export const boosterData = {
-
-}
+export const boosterData = []
