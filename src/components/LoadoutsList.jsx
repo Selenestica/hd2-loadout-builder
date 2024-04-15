a8413ee72b877d0aad674bf4f8ca612d947dc030
PeterDavids <peter@rawstadia.com>
2024-04-15 14:48:20 +0200

    loadoutlist

--- src/components/LoadoutsList.jsx
+++ src/components/LoadoutsList.jsx
@@ -1,18 +1,54 @@
 import { useState } from 'react'
 import { strategemData } from '../data/hardcodedData'
 import { css } from '@emotion/css'
+import { colors } from '../data/constants'
 
-export default function LoadoutsList() {
+export default function LoadoutsList({ handleClick }) {
+    const [hoverState, setHoverState] = useState({})
+
     return (
-        <div>
-            {strategemData.toSorted((a,b) => a.type > b.type).map(strat => {
-                return <div className={css`
-                display: flex; 
-                `}>
-                    <img src={strat.icon} alt={strat.name} />
-                    {strat.name}
-                </div>
-            })}
-        </div >
+        <div className={css`
+            display: grid;
+            width: 100%;
+            grid-template: auto / 20em 1fr;
+        `}>
+            <div className={css`
+                background: ${colors.darkBlue};
+                max-height: 80vh;
+                overflow-Y: auto;
+            `}>
+                {strategemData.toSorted((a, b) => (
+                    a.color !== b.color ?
+                        a.color > b.color
+                        : a.type > b.type
+                )).map(strat => {
+                    return <div className={css`
+                        display: flex; 
+                        align-items: center; 
+                        gap: 1em;
+                        padding-right: 1em;
+                        cursor: pointer;
+                        white-space: nowrap;
+                        
+                        &:hover {
+                            background: ${colors.lightBlue};
+                        }
+                    `}
+                        onClick={() => handleClick(strat)}
+                        onPointerEnter={() => setHoverState(strat)}
+                    >
+                        <img src={strat.icon} alt={strat.name} />
+                        {strat.name}
+                    </div>
+                })}
+            </div >
+            <div className={css`
+                padding: 1em;
+                background: rgba(255,255,255, 0.1);
+                max-width: 20em;
+            `}>
+                {JSON.stringify(hoverState)}
+            </div>
+        </div>
     )
 }
---
+++ src/data/constants.js
@@ -0,0 +1,5 @@
+
+export const colors = {
+    darkBlue:  'rgba(023,056,090, 0.5)',
+    lightBlue: 'rgba(023,056,090, 1)'
+}
