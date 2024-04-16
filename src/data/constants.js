88e689713101849710c58c417e7dd8f6d67bb300
PeterDavids <peter@rawstadia.com>
2024-04-16 11:04:53 +0200

    fsdfsdf

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,5 +2,6 @@
 export const colors = {
     darkBlue:  'rgba(023,056,090, 0.5)',
     lightBlue: 'rgba(023,056,090, 1)',
-    lighter: 'rgba(255,255,255, 0.1)'
+    lighter: 'rgba(255,255,255, 0.1)',
+    
 }
