6fb82e16a8a9ea26b22b360aa90dbcbdd4f144c5
Medusil <peterdavids.red@gmail.com>
2024-05-06 19:42:36 +0200

    add donation footer

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ export default function App() {
                 }
 
                 @media screen and (max-width: 800px) {
+                    height: auto;
                     grid-template: 50vh max-content max-content / 1fr;
-                    flex-direction: column;
-                    padding: 1em 0.5em 0.5em 0.5em;
-
+                    padding: 1em 0.5em 2em 0.5em;
+                    margin-bottom: 1em;
                 }
             `}>
                 <LoadoutList />
@@ -33,10 +33,46 @@ export default function App() {
                 <div className={css`
                     position: fixed;
                     bottom: 0;
-                    left: 2em;
-                    opacity: 0.2;
+                    left: 0;
+                    padding-left: 2em;
+                    display: flex;
+                    height: 2em;
+                    overflow: hidden;
+                    width: 100%;
+                    display: flex;
+                    place-items: center;
+                    background: rgb(25,25,25);
+                    color: rgba(255,255,255,0.2);
                 `}>
+                    <div>
+
+                    </div>
                     v1.000.300
+
+                    <span className={css`
+                        margin-left: 1em;
+                        cursor: pointer;
+                        text-decoration: underline;
+
+                        &: hover{
+                            color: yellow;
+                        }
+                    `}>Support us by donating!</span>
+
+                    <span className={css`
+                        margin-left: 1em;
+                        cursor: pointer;
+                        text-decoration: underline;
+
+                        &: hover{
+                            color: yellow;
+                        }
+                    `}>View supporters</span>
+                    {/* <form action="https://www.paypal.com/donate" method="post" target="_top" className={css`scale: 0.5;`}>
+                        <input type="hidden" name="hosted_button_id" value="S4PWZALBVH6NU" />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                        <img alt="" border="0" src="https://www.paypal.com/en_BE/i/scr/pixel.gif" width="1" height="1" />
+                    </form> */}
                 </div>
             </div>
             <Outlet />
