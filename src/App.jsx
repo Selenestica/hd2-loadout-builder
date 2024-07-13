c2a8cc6f67a245330fe1652fff7d6707bb61b466
Medusil <peterdavids.red@gmail.com>
2024-07-13 15:50:19 +0200

    v1.000.405

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ export default function App() {
                         place-items: center;
                         color: rgba(255,255,255,0.2);
                     `}>
-                    v1.000.400
+                    v1.000.405
 
                     <span className={css`
                             margin-left: 1em;
