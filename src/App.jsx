f264f7ef97d9c695b11c57f905a32159a4f515a6
Medusil <peterdavids.red@gmail.com>
2025-07-19 13:34:20 +0200

    control group warbond add version number

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ export default function App() {
                         place-items: center;
                         color: rgba(255,255,255,0.2);
                     `}>
-                    v1.003.102
+                    v1.003.201
 
                     <span className={css`
                             margin-left: 1em;
