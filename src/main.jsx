ae345fda7bc1b04aed1fcb55ba02d91143a39a89
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2026-01-14 11:32:52 -0500

    adjusted app routes to work with main site

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { LoadoutsProvider } from './context/Loadouts.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ReceivedLoadoutModal from './components/ReceiveLoadoutModal.jsx'
-import Modal from 'react-modal'
-import { OverridesProvider } from './context/Overrides.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { LoadoutsProvider } from "./context/Loadouts.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReceivedLoadoutModal from "./components/ReceiveLoadoutModal.jsx";
+import Modal from "react-modal";
+import { OverridesProvider } from "./context/Overrides.jsx";
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <LoadoutsProvider >
-        <OverridesProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<App />} >
-                        <Route path="s" element={<ReceivedLoadoutModal />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </OverridesProvider>
-    </LoadoutsProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <LoadoutsProvider>
+    <OverridesProvider>
+      <BrowserRouter basename="/loadoutbuilder">
+        <Routes>
+          <Route path="/*" element={<App />}>
+            <Route path="s" element={<ReceivedLoadoutModal />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </OverridesProvider>
+  </LoadoutsProvider>
+);
