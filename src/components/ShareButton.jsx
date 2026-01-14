50ff4dd2c99f64e6979915e4c961c8954e165d13
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2026-01-14 16:05:57 -0500

    minor changes to allow for loadout sharing

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import ClipboardJS from 'clipboard'
+import React, { useState, useEffect, useMemo } from "react";
+import ClipboardJS from "clipboard";
 
 export default function ShareButton({ newLoadout, ...props }) {
-    const [copied, setCopied] = useState(false)
+  const [copied, setCopied] = useState(false);
 
-    const factionMapping = ['Generic', 'Bugs', 'Bots', 'Illuminate']
-    const factionIndex = factionMapping.indexOf(newLoadout.faction)
+  const factionMapping = ["Generic", "Bugs", "Bots", "Illuminate"];
+  const factionIndex = factionMapping.indexOf(newLoadout.faction);
 
-    const clipboardText = useMemo(() => {
-        const encodedName = encodeURIComponent(newLoadout.name)
-        const encodedData = [
-            newLoadout.strat1 || 'x',
-            newLoadout.strat2 || 'x',
-            newLoadout.strat3 || 'x',
-            newLoadout.strat4 || 'x',
-            newLoadout.primary || 'x',
-            newLoadout.secondary || 'x',
-            newLoadout.grenade || 'x',
-            newLoadout.armor || 'x',
-        ].join('i')
-        return `${window.location.origin}/s?n=${encodedName}&d=${encodedData}&f=${factionIndex}`
-    }, [newLoadout])
+  const clipboardText = useMemo(() => {
+    const encodedName = encodeURIComponent(newLoadout.name);
+    const encodedData = [
+      newLoadout.strat1 || "x",
+      newLoadout.strat2 || "x",
+      newLoadout.strat3 || "x",
+      newLoadout.strat4 || "x",
+      newLoadout.primary || "x",
+      newLoadout.secondary || "x",
+      newLoadout.grenade || "x",
+      newLoadout.armor || "x",
+    ].join("i");
+    return `${window.location.origin}/loadoutbuilder/s?n=${encodedName}&d=${encodedData}&f=${factionIndex}`;
+  }, [newLoadout]);
 
-    useEffect(() => {
-        const clipboard = new ClipboardJS('#btn-copy', {
-            text: () => clipboardText
-        })
+  useEffect(() => {
+    const clipboard = new ClipboardJS("#btn-copy", {
+      text: () => clipboardText,
+    });
 
-        clipboard.on('success', function(e) {
-            setCopied(true)
-            setTimeout(() => setCopied(false), 2000)  // Reset after 2 seconds
-            e.clearSelection()
-        })
+    clipboard.on("success", function (e) {
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+      e.clearSelection();
+    });
 
-        clipboard.on('error', function() {
-            // Handle errors (not typically necessary unless you support very old browsers)
-        })
+    clipboard.on("error", function () {
+      // Handle errors (not typically necessary unless you support very old browsers)
+    });
 
-        return () => {
-            clipboard.destroy()
-        }
-    }, [clipboardText])
+    return () => {
+      clipboard.destroy();
+    };
+  }, [clipboardText]);
 
-    return (
-        <button id="btn-copy" {...props} >
-            {copied ? 'Link Copied!' : 'Share url'}
-        </button>
-    )
+  return (
+    <button id="btn-copy" {...props}>
+      {copied ? "Link Copied!" : "Share url"}
+    </button>
+  );
 }
