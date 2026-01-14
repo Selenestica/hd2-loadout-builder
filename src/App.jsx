5058f71878fb7c08ddbb93c2441fb23817e71d0f
JoeBen Wilson <53917177+Selenestica@users.noreply.github.com>
2026-01-14 13:05:31 -0500

    adjusted footer text

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,108 @@
-import { useContext, useState } from 'react'
-import { css } from '@emotion/css'
-import LoadoutsContext from './context/Loadouts'
-import LoadoutList from './components/LoadoutList'
-import LoadoutDetails from './components/LoadoutDetails'
-import { Outlet } from 'react-router-dom'
-import NameScroller from './components/NameScroller'
-import backgroundUrl from './assets/backgrounds/super-earth-flag.svg'
-import videoUrl from './assets/backgrounds/particles2.mp4'
-import DonateModal from './components/DonateModal'
+import { useContext, useState } from "react";
+import { css } from "@emotion/css";
+import LoadoutsContext from "./context/Loadouts";
+import LoadoutList from "./components/LoadoutList";
+import LoadoutDetails from "./components/LoadoutDetails";
+import { Outlet } from "react-router-dom";
+import NameScroller from "./components/NameScroller";
+import backgroundUrl from "./assets/backgrounds/super-earth-flag.svg";
+import videoUrl from "./assets/backgrounds/particles2.mp4";
+import DonateModal from "./components/DonateModal";
 
 export default function App() {
-    const { selectedLoadout } = useContext(LoadoutsContext)
-    const [showDonateModal, setShowDonateModal] = useState(false)
+  const { selectedLoadout } = useContext(LoadoutsContext);
 
-    return (
-        <>
-            <div className={css`
-                display: grid; 
-                height: 100%;
-                grid-template-rows: 1fr 2em;
-                background: url(${backgroundUrl}) no-repeat center / 80% auto;
-                position: relative;
-            `}>
-                {<video autoPlay loop muted playsInline className={css`
-                    width: 100%;
-                    height: 100%;
-                    object-fit: cover;
-                    position: absolute;
-                    z-index: -1;
-                    opacity: 0.2;
+  return (
+    <>
+      <div
+        className={css`
+          display: grid;
+          height: 100%;
+          grid-template-rows: 1fr 2em;
+          background: url(${backgroundUrl}) no-repeat center / 80% auto;
+          position: relative;
+        `}
+      >
+        {
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className={css`
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              position: absolute;
+              z-index: -1;
+              opacity: 0.2;
 
-                    @media (max-width: 768px) { 
-                        display: none;
-                    }
-                `}>
-                    <source src={videoUrl} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>}
+              @media (max-width: 768px) {
+                display: none;
+              }
+            `}
+          >
+            <source src={videoUrl} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        }
 
-                <div className={css`
-                    display: grid;
-                    grid-template-columns: 1.2fr 1fr 1fr;
-                    grid-gap: 1em;
-                    padding: 2em 2em 0em 2em;
-                    height: 100%;
-                    overflow: auto;
-                    
-                    @media screen and (max-width: 1400px) {
-                        grid-template: 1.2fr 0.8fr / 1fr 1fr;
-                    }
-                    
-                    @media screen and (max-width: 800px) {
-                        height: auto;
-                        grid-template: 50vh max-content max-content / 1fr;
-                        padding: 1em 0.5em 2em 0.5em;
-                        margin-bottom: 1em;
-                    }
+        <div
+          className={css`
+            display: grid;
+            grid-template-columns: 1.2fr 1fr 1fr;
+            grid-gap: 1em;
+            padding: 2em 2em 0em 2em;
+            height: 100%;
+            overflow: auto;
 
-                    >div {
-                        background: rgba(180,200,250,0.15);
-                    }
-                `}>
-                    <LoadoutList />
-                    {selectedLoadout && <LoadoutDetails />}
-                </div>
+            @media screen and (max-width: 1400px) {
+              grid-template: 1.2fr 0.8fr / 1fr 1fr;
+            }
 
-                <div className={css`
-                        padding-left: 2em;
-                        display: flex;
-                        height: 100%;
-                        overflow: hidden;
-                        width: 100%;
-                        display: grid;
-                        grid-template-columns: auto auto auto 1fr;
-                        place-items: center;
-                        color: rgba(255,255,255,0.2);
-                    `}>
-                    v1.005.001
+            @media screen and (max-width: 800px) {
+              height: auto;
+              grid-template: 50vh max-content max-content / 1fr;
+              padding: 1em 0.5em 2em 0.5em;
+              margin-bottom: 1em;
+            }
 
-                    <span className={css`
-                            margin-left: 1em;
-                            cursor: pointer;
-                            text-decoration: underline;
-                            white-space: nowrap;
-                            
-                            &: hover{
-                                color: yellow;
-                            }
-                        `}
-                        onClick={() => setShowDonateModal(true)}
-                    >Support us by donating!</span>
+            > div {
+              background: rgba(180, 200, 250, 0.15);
+            }
+          `}
+        >
+          <LoadoutList />
+          {selectedLoadout && <LoadoutDetails />}
+        </div>
 
-                    <span className={css`
-                            margin-left: 1em;
-                        `}>
-                        Thanks to:&nbsp;
-                    </span>
-                    <NameScroller />
-                    {showDonateModal && <DonateModal setShowDonateModal={setShowDonateModal}/>}
-                </div>
-            </div>
-            <Outlet />
-        </>
-    )
+        <div
+          className={css`
+            padding-left: 2em;
+            display: flex;
+            height: 100%;
+            overflow: hidden;
+            width: 100%;
+            display: grid;
+            grid-template-columns: auto auto auto 1fr;
+            place-items: center;
+            color: rgba(255, 255, 255, 0.2);
+          `}
+        >
+          v1.005.001
+          <span
+            className={css`
+              margin-left: 1em;
+            `}
+          >
+            Thanks to Ultros and their supporters:&nbsp;
+          </span>
+          <NameScroller />
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
 // TO DO:
